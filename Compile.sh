9031d9afdaa1c9ccc0c2ce68307a316c8c6815d1
HICKEY John <John.Hickey@roslin.ed.ac.uk>
2014-01-15 16:07:38 +0000

    Compile.sh and .gitignore

--- Compile.sh
+++ Compile.sh
@@ -1,1 +1,14 @@
-ifort *.f90 -mkl -O3 -i-static -O3 -m64 -o AlphaBayes3
+ifort Global.f90 -c -O3 -i-static -O3 -m64
+ifort ReadData.f90 -c -O3 -i-static -O3 -m64
+ifort ReadParam.f90 -c -O3 -i-static -O3 -m64
+ifort InitiateSeed.f90 -c -O3 -i-static -O3 -m64
+ifort PearsnR4.f90 -c -O3 -i-static -O3 -m64
+ifort RidgeRegression.f90 -c -O3 -i-static -O3 -m64
+ifort gasdev.f90 -c -O3 -i-static -O3 -m64
+ifort momentR4.f90 -c -O3 -i-static -O3 -m64
+ifort ran1.f90 -c -O3 -i-static -O3 -m64
+ifort random_order.f90 -c -O3 -i-static -O3 -m64
+ifort AlphaBayes.f90 -c -O3 -i-static -O3 -m64
+
+
+ifort *.o -mkl -o AlphaBayes3
