347120af3514b058b9b3c5fd830304648b7b6122
Gregor Gorjanc <gregor.gorjanc@gmail.com>
2015-06-19 15:50:32 +0100

    Added the missing definition of GenoTeId to be able to compile

--- Global.f90
+++ Global.f90
@@ -5,7 +5,7 @@ implicit none
 
 integer :: idum,nSnp,nSnpExternal,nAnisTr,nAnisTe,nRound,nBurn,nProcessors,ScalingOpt,MissingGenoCode
 real(4) :: VarY,VarA,VarE,Mu,Sum2pq
-real(4),allocatable,dimension(:) :: SnpTmp,Lambda,SnpOut,AlleleFreq
+real(4),allocatable,dimension(:) :: SnpTmp,Lambda,SnpOut,AlleleFreq,GenoTeId
 real(4),allocatable,dimension(:,:) :: GenosTr,GenosTe,Phen,E,G,Ebv,Tbv,XpX,Xg
 integer,allocatable,dimension(:) :: FixedSnp,SnpPosition
 
