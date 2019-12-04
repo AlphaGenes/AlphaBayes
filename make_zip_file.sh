b2ca82165433424fb605b018564c0c1bce2838dd
WHALEN Andrew <awhalen@mvm-ri-m155130.roslin.ed.ac.uk>
2019-12-04 10:12:20 +0000

    re-adding makefile

---
+++ make_zip_file.sh
@@ -0,0 +1,28 @@
+mkdir AlphaBayes
+
+# Assumes that the program and manual have both been built.
+
+# To build the program run:
+# NOTE: Binaries should be moved to the "binaries" folder and uploaded to bitbucket after builds.
+#cmake . ; make
+
+# to build the manual using Sphinx:
+#( cd doc ; make latexpdf )
+
+cp -r example AlphaBayes
+
+# Copy in the documentation.
+cp doc/build/latex/AlphaBayes.pdf AlphaBayes/AlphaBayes.pdf
+
+# Copy in the binaries
+cp binaries/* AlphaBayes
+
+# Create a version file
+
+version=`git describe --tags --abbrev=0`
+commit=`git rev-parse --short HEAD`
+
+echo Version: $version > AlphaBayes/version.txt
+echo Commit: $commit >> AlphaBayes/version.txt
+
+zip -r AlphaBayes.zip AlphaBayes
